c92d25508d403af8863cee5a46fcd3c091315507
bratyshevTD <bratyshev2013@mail.ru>
2022-10-03 03:22:19 +0300

    Анимация фигур

--- src/FrameMain.java
+++ src/FrameMain.java
@@ -1,32 +1,88 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class FrameMain extends JPanel {
+    private int x1 = 50;
+    private int y1 = 400;
+    private int x2 = 500;
+    Color color1;
+    private boolean flagWight = true;
+    private boolean flagHeight = true;
+    FrameMain() {
+        Timer timer = new Timer(1, new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                repaint();
+            }
+        });
+        timer.start();
+    }
+
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+
+
         super.paint(g2);
-//        g2.setBackground(Color.BLACK);
-
-
-        g2.setColor(Color.ORANGE);
-        g2.fillOval(100, 350, 75, 50);
+        g2.setBackground(Color.BLACK);
 
         g2.setColor(Color.BLACK);
         g2.drawLine(0, 250, 700, 250);
+        g2.drawLine(325,0,325,700);
+
+        g2.setColor(Color.ORANGE);
+        g2.fillOval(x1, 400, 75, 50);
 
         g2.setColor(Color.RED);
-        g2.fill3DRect(50, 100, 50, 100, true);
+        g2.fill3DRect(50, y1, 50, 100, true);
 
-        g2.setColor(Color.MAGENTA);
-        g2.fillArc(100, 200, 200, 100, 0, 90);
+
+        g2.setColor(color1);
+        g2.fillArc(225, 200, 200, 100, 0, 90);
+
 
         g2.setColor(Color.BLACK);
-        g2.fillArc(100, 200, 200, 100, 90, 90);
+        g2.fillArc(225, 200, 200, 100, 90, 90);
 
         g2.setColor(Color.GREEN);
-        g2.fillArc(100, 200, 200, 100, 180, 180);
+        g2.fillArc(225, 200, 200, 100, 180, 90);
+
+        g2.setColor(Color.BLUE);
+        g2.fillArc(225, 200, 200, 100, 270, 90);
+
+        g2.setColor(Color.blue);
+        g2.fillOval(x2, 50, 75, 50);
+
+        if (flagWight) {
+            x1++;
+            x2--;
+            if (x1 == 500)
+                flagWight = false;
+        } else {
+            x1--;
+            x2++;
+            if (x1 == 50) {
+                flagWight = true;
+            }
+        }
+        if(flagHeight) {
+            y1--;
+            if (y1 == -10)
+                flagHeight = false;
+        } else {
+            y1++;
+            if (y1 == 400)
+                flagHeight = true;
+            color1 = Color.MAGENTA;
+        }
+
+
 
 
 
     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Простейшие геометрические объекты");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         FrameMain gui = new FrameMain();
-        gui.setBackground(Color.pink);
+        gui.setBackground(Color.PINK);
         frame.add(gui);
-        frame.setSize(700, 500);
+        frame.setSize(700, 600);
         frame.setVisible(true);
         frame.setResizable(false);
 
